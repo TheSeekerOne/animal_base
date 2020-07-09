c72bc2bd57ba96d2e8699c82122129b1b626faa4
18shevkov <test.testo@testo.test>
2020-07-09 11:25:21 +0400

    add Animal model

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Animal(models.Model):
+    """Модель животного находящегося в приюте"""
+    name = models.CharField(max_length=40)
+    age = models.IntegerField()
+    arrival_date = models.DateField(default=date.today)
+    weight = models.DecimalField(max_digits=5, decimal_places=1)
+    height = models.DecimalField(max_digits=4, decimal_places=2)
+    spec_features = models.TextField()
